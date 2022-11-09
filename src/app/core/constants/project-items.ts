4fab390e178bc23fa8e43aaf26422394c2a3dab4
Taylor Buckner <tabuckner@Taylors-MacBook-Pro.local>
2022-11-09 17:04:24 -0600

    added images to all project cards.

--- src/app/core/constants/project-items.ts
+++ src/app/core/constants/project-items.ts
@@ -5,75 +5,88 @@ export const PROJECT_ITEMS: ProjectItemModel[] = [
     title: 'Material-DayJs-Adapter',
     description: 'A DayJs based datepicker adapter for the @angular/material library that significantly reduces dependency size.',
     sourceUrl: 'https://github.com/tabuckner/material-dayjs-adapter',
-    demoUrl: 'https://angular.io/resources?category=development#:~:text=material%2Ddayjs%2Dadapter'
+    demoUrl: 'https://angular.io/resources?category=development#:~:text=material%2Ddayjs%2Dadapter',
+    imageUrl: '/assets/img/projects/material-dayjs-adapter.png',
   },
   {
     title: 'MDP Sass',
     description: 'A SASS/SCSS helper for generating an @angular/material compatible Material Pallette. Useful for custom themes.',
     sourceUrl: 'https://github.com/tabuckner/mdp-sass',
-    demoUrl: 'https://stackblitz.com/edit/angular-ivy-ddtzum?file=src/styles.scss'
+    demoUrl: 'https://stackblitz.com/edit/angular-ivy-ddtzum?file=src/styles.scss',
+    imageUrl: '/assets/img/projects/mdp-sass.png',
   },
   {
     title: 'Lazy VIN Lib',
     description: 'A zero-dependency TypeScript based library for generating and validating random VIN numbers.',
     sourceUrl: 'https://github.com/tabuckner/lazy-vin-lib',
-    demoUrl: 'https://stackblitz.com/edit/lazy-vin-lib-demo?file=src%2Fapp%2Fapp.component.ts'
+    demoUrl: 'https://stackblitz.com/edit/lazy-vin-lib-demo?file=src%2Fapp%2Fapp.component.ts',
+    imageUrl: '/assets/img/projects/lazy-vin-lib.jpeg',
   },
   {
     title: 'Doggo-Translator-TS',
     description: 'A small library for synchronously translating strings and string-partials in to \'doggo\'.',
     sourceUrl: 'https://github.com/tabuckner/doggo-translator-ts',
-    demoUrl: 'https://stackblitz.com/edit/react-ts-2hqzlh?file=App.tsx'
+    demoUrl: 'https://stackblitz.com/edit/react-ts-2hqzlh?file=App.tsx',
+    imageUrl: '/assets/img/projects/doggo-translator-ts.png',
   },
   {
     title: 'Chimes',
     description: 'A simple web app combining music and visualizations. Fun to play around with!',
     sourceUrl: 'https://github.com/tabuckner/chimes',
-    demoUrl: 'https://tabuckner.github.io/chimes/'
+    demoUrl: 'https://tabuckner.github.io/chimes/',
+    imageUrl: '/assets/img/projects/chimes.png',
   },
   {
     title: 'Barcode Tool',
     description: 'A simple web app for working with random VIN numbers. Useful for those working in the automotive tech space!',
     sourceUrl: 'https://github.com/tabuckner/barcode-tool',
-    demoUrl: 'https://tabuckner.github.io/chimes/barcode-tool'
+    demoUrl: 'https://tabuckner.github.io/barcode-tool',
+    imageUrl: '/assets/img/projects/barcode-tool.png',
   },
   {
     title: 'Random Contrast Compliant',
     description: 'SASS/SCSS helper for generating random colors that meet a user-defined WCAG 2.0 contrast compliance level.',
     sourceUrl: 'https://github.com/tabuckner/random-contrast-compliant',
+    imageUrl: '/assets/img/projects/random-contrast-compliant.png'
   },
   {
     title: 'Random Contrast Compliant TS',
     description: 'TypeScript helper for generating random colors that meet a user-defined WCAG 2.0 contrast compliance level.',
     sourceUrl: 'https://github.com/tabuckner/random-contrast-compliant-ts',
-    demoUrl: 'https://stackblitz.com/edit/react-ts-npanwi?file=App.tsx'
+    demoUrl: 'https://stackblitz.com/edit/react-ts-npanwi?file=App.tsx',
+    imageUrl: '/assets/img/projects/random-contrast-compliant-ts.png'
   },
   {
     title: 'Spongebob Case',
     description: 'A tiny, zero-dependency library for converting strings into ThE PoPuLaR MeMe CaSe!',
     sourceUrl: 'https://github.com/tabuckner/spongebob-case',
-    demoUrl: 'https://stackblitz.com/edit/angular-wpb9a4?file=src%2Fapp%2Fapp.component.ts'
+    demoUrl: 'https://stackblitz.com/edit/angular-wpb9a4?file=src%2Fapp%2Fapp.component.ts',
+    imageUrl: 'https://imgflip.com/s/meme/Mocking-Spongebob.jpg'
   },
   {
     title: 'Gulp Materialize',
     description: 'Boilerplate repo for quick Gulp projects using Materialize.',
     sourceUrl: 'https://github.com/tabuckner/gulp-materialize',
+    imageUrl: '/assets/img/projects/gulp-materialize.png'
   },
   {
     title: 'Bible Thumper Twitter Bot',
     description: 'A twitter-bot that makes inspirational scripture based posts, and responds to chat messages.',
     sourceUrl: 'https://github.com/tabuckner/bible-thumper-twitter-bot',
-    demoUrl: 'https://twitter.com/hillbillynums'
+    demoUrl: 'https://twitter.com/hillbillynums',
+    imageUrl: '/assets/img/projects/bible-thumper-twitter-bot.png'
   },
   {
     title: 'Time Tracker',
     description: 'A tiny anonymously authenticated web app for tracking time punches.',
     sourceUrl: 'https://github.com/tabuckner/time-tracker',
-    demoUrl: 'https://tabuckner.github.io/time-tracker/'
+    demoUrl: 'https://tabuckner.github.io/time-tracker/',
+    imageUrl: '/assets/img/projects/time-tracker.png'
   },
   {
     title: 'Jenkins Sucks',
     description: 'A Slack App aimed at helping problematic Jenkins services scan repositories on command.',
     sourceUrl: 'https://github.com/tabuckner/jenkins-sucks',
+    imageUrl: '/assets/img/projects/jenkins-sucks.png'
   },
 ];
